560d02ba48366d158a4c7c2de751545f517b39af
Ryan Boepple <ryanboepple@creighton.edu>
2019-10-13 18:48:53 -0500

    did the first requirement

---
+++ Fixit9RyanBoepple.appstudio/forms/membership/membership.js
@@ -0,0 +1,18 @@
+/*
+var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+
+btnSubmit1.onclick=function(){
+  var isMember= members.includes(inptFirstName.value)
+  if (isMember==true){
+    lblCheck.value=`Hi ${inptFirstName.value} you are a member.`
+  }
+  else{
+    lblCheck.value=`Hi ${inptFirstName.value} you are not a member, but we will make you one now.`
+    members.push(inptFirstName.value)
+  }
+}
+*/
+
+  
+  
